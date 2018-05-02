fc29247174229b0c2308ef4730defcc6538e4780
Laura Bodell <bodelllaura@gmail.com>
2018-05-02 16:07:31 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -10,6 +10,10 @@ post '/team' do
  @name= params["name"]
  @coach= params["coach"]
  @pointguard= params["pg"]
+ @shootingguard= params["sg"]
+ @powerforward= params["pf"]
+ @smallforward= params["sf"]
+ @center= params["c"]
  
   
   erb :team 
